5189467982d2ed2637e6c0c0717b3024bc03de53
Chamsedine AIDARA <50946856+chamsdg@users.noreply.github.com>
2022-07-27 12:47:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-## Importation des librarys 
+## Importation des libraries
 import joblib
 #import uvicorn
 from flask import Flask, jsonify
@@ -9,33 +9,31 @@ from lightgbm import LGBMClassifier
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-
+# path data
 PATH = 'dataset/'
-#      Chargement des données 
 
-#    data = pd.read_csv('dataset/test_dataframe.csv')
+
+# data = pd.read_csv('dataset/test_dataframe.csv')
 data = pd.read_parquet('test_df.parquet')
 print('la taille de Dataframe est = ', data.shape)
 
-#data.drop(columns = {'Unnamed: 0'}  , inplace = True)
+# size of DataFrame
 
 print('la taille de la nouvelle Dataframe est = ', data.shape)
-#Chargement du modèle
-# model = pickle.load(open('model/ModelClassifier.pkl', 'rb'))
+
+# Chargement du modèle
+
 model = pickle.load(open('ModelClassifier.pkl', 'rb'))
 
 
 
-#app = flask.Flask(__name__)
+# app flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello Everyone ,  This is my Web API for credit scoring "
-
-
-
 
 
 @app.route('/credit/<id_client>')
@@ -63,43 +61,26 @@ def credit(id_client):
     
     proba = model.predict_proba(X)
     prediction = model.predict(X)
-    ## autre = model.
+    
  
     #DEBUG
+    
     print('L''identificateur du client : ', id_client)
   
     dict_final = {
         'prediction' : int(prediction),
-        'proba' : float(proba[0][0])
-        ##  Ajouter d'autres parametres 
+        'proba' : float(proba[0][0]) 
         }
    
     print('Lancer une nouvelle Prédiction : \n', dict_final)
     
 
-     # Say=uvegarde le résultat sous forme de JSON file 
+     # Sauvegarder le résultat sous forme de JSON file 
         
     return jsonify(dict_final)
 
 
-#  lancement de l'application   (  mode local  et non en mode production  ) 
-
-#def create_app():
-#       return app
-
-
+#  lancement de l'application   (mode local  et non en mode production) 
 if __name__ == "__main__":
-    #app.secret_key = "krankenhouse"
-    #waitress.serve(myapp.wsgifunc, port=8041, url_scheme='https')
-    ## from waitress import serve
-    #app.run(host = 'localhost', port = 8088, debug = True)
-    ## serve(app, host="127.0.0.1", port=8000)
-    #uvicorn.run(app = '127.0.0.1', port = 8000, debug = True)
-    ##app.run(host = 'localhost', port = 8088, debug = True)
-    ## from waitress import serve
-    #app.run(host = '127.0.0.1', port = 5000, debug = True)
-    #http://127.0.0.1:5000
-    ##serve(app, host="0.0.0.0", port=8080)
-    ##app.run(debug=True)
     app.debug = True
     app.run()
